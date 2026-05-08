6d35841074f0277fdf52e9a4510f88b534088526
Ksaira <K761141021@gmail.com>
2026-05-08 19:08:22 +0800

    实现后端

---
+++ backend/web/views/user/account/logout.py
@@ -0,0 +1,14 @@
+from django.http import response
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]  #强制必须登录才能访问
+    def post(self, request):
+        reponse = Response({
+            'result': 'success',
+        })
+        response.delete_cookie('refresh_token')
+        return reponse
