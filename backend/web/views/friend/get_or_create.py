5b0df37ca45f170289395c5f820b8051f69d92fa
Ksaira <K761141021@gmail.com>
2026-05-26 19:34:22 +0800

    聊天框前端

--- backend/web/views/friend/get_or_create.py
+++ backend/web/views/friend/get_or_create.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import AbstractUser
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser, IsAuthenticated
-from web.models.character import Character
+from rest_framework.permissions import IsAuthenticated
 from web.models.friend import Friend
 from web.models.user import UserProfile
-from web.views.create import character
 
 
 class GetOrCreateFriendView(APIView):
