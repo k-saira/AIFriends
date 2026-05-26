0afb835128fa9a37dbdc506398e064495191069a
Ksaira <K761141021@gmail.com>
2026-05-26 22:46:26 +0800

    前端实现

--- backend/web/views/friend/get_list.py
+++ backend/web/views/friend/get_list.py
@@ -2,14 +2,13 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated
 from web.models.friend import Friend
-from web.views.create import character
 
 
 class GetListFriendView(APIView):
     permission_classes = [IsAuthenticated]
     def get(self, request):
         try:
-            items_count = int(request.query_params.get('item-count', 0))
+            items_count = int(request.query_params.get('items_count', 0))
             friend_raw = Friend.objects.filter(me__user=request.user).order_by('-update_time')[items_count:items_count + 20]
             friends = []
             for friend in friend_raw:
@@ -30,10 +29,10 @@ class GetListFriendView(APIView):
                         }
                     }
                 })
-                return Response({
-                    'result': 'success',
-                    'friend_list': friends,
-                })
+            return Response({
+                'result': 'success',
+                'friends': friends,
+            })
         except:
             return Response({
                 'result': '系统异常，请稍后再试',
