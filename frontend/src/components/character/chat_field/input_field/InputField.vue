f36c635c32425bd4a0fb88a8e526812fe3b51482
Ksaira <K761141021@gmail.com>
2026-06-04 10:01:22 +0800

    小布丁

--- frontend/src/components/character/chat_field/input_field/InputField.vue
+++ frontend/src/components/character/chat_field/input_field/InputField.vue
@@ -17,11 +17,11 @@ function focus() {
 }
 
 async function handleSend() {
-  if (isProcessing) return
-  isProcessing = true
-
   const content = message.value.trim()
   if (!content) return
+
+  if (isProcessing) return
+  isProcessing = true
   message.value = ''
 
   emit('pushBackMessage',{role: 'user', content: content, id: crypto.randomUUID()})
