9f53fac1a0b222ad59e3c2a687bf77ad982fa65e
Ksaira <K761141021@gmail.com>
2026-06-11 11:41:29 +0800

    修改前端路由bug

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,7 +11,7 @@ import {useUserStore} from "@/stores/user.js";
 import UpdateCharater from "@/views/create/character/UpdateCharater.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/'),
   routes: [
     {
       path: '/',
